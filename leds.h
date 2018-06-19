af52681154484c8f02576299549acbfdc4995493
Jimmy Smith <jimmy@doomkeeper.com>
2018-06-18 18:14:21 -0700

    working irq handler, basic lights

---
+++ leds.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include "FastLED.h"
+
+// suit is 5m, 32 leds per meter.
+#define NUM_LEDS 32*5
+#define DATA_PIN 9
+#define CLOCK_PIN 10
+
+// Define the array of leds
+CRGB leds[NUM_LEDS];
+
+void fadeall() { for (int i = 0; i < NUM_LEDS; i++) { leds[i].nscale8(250); } }
+
+void setupLeds()
+{
+	LEDS.addLeds<APA102, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
+	LEDS.setBrightness(84);
+}
+
+void doLeds()
+{
+	// Copy paste of Cylon code.
+	static uint8_t hue = 0;
+	//Serial.print("x");
+	// First slide the led in one direction
+	for (int i = 0; i < NUM_LEDS; i++) {
+		// Set the i'th led to red 
+		leds[i] = CHSV(hue++, 255, 255);
+		// Show the leds
+		FastLED.show();
+		// now that we've shown the leds, reset the i'th led to black
+		// leds[i] = CRGB::Black;
+		fadeall();
+		// Wait a little bit before we loop around and do it again
+		delay(10);
+	}
+	Serial.print("x");
+
+	// Now go in the other direction.  
+	for (int i = (NUM_LEDS)-1; i >= 0; i--) {
+		// Set the i'th led to red 
+		leds[i] = CHSV(hue++, 255, 255);
+		// Show the leds
+		FastLED.show();
+		// now that we've shown the leds, reset the i'th led to black
+		// leds[i] = CRGB::Black;
+		fadeall();
+		// Wait a little bit before we loop around and do it again
+		delay(10);
+	}
+}
